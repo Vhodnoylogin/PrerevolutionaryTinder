786f87efc734ca167b4f1bfa5febbec2a1d72787
Facnet <zabuza53@yandex.ru>
2023-04-23 18:01:29 +0300

    Попытка в кнопки

---
+++ clientPart/src/main/java/ru/liga/BotConfig.java
@@ -0,0 +1,6 @@
+package ru.liga;
+
+public class BotConfig {
+    public static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+    public static final String BOT_NAME = System.getenv("BOT_NAME");
+}
---
+++ clientPart/src/main/java/ru/liga/Main.java
@@ -0,0 +1,18 @@
+package ru.liga;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.liga.updatehandler.TinderHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new TinderHandler());
+        } catch (TelegramApiException e) {
+            //logger.error(String.valueOf(e));
+            throw new RuntimeException(e);
+        }
+    }
+}
